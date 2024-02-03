9bb097cb5140bffd70589f4bec2fe434aa6adec7
Александра Пупкина (Угрюмова) <124123433+AlexandraUgryumova@users.noreply.github.com>
2024-02-03 11:02:18 +0500

    initial commit

---
+++ практическая 3/практическая 3.cpp
@@ -0,0 +1,45 @@
+﻿// практическая 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<conio.h>
+using namespace std;
+#include<math.h>
+
+//Задание 1
+//int x;
+//#define a (1)/(sqrt(x)+sqrt(2))
+//#define z (sqrt(a)+sqrt(2))/(a+2) 
+
+//Задание 2
+//int z;
+//#define x -4.11
+//#define y 2.99
+//#define w (y*sqrt(pow(x+y,4)))/(x+10*y)
+//#define f (log10(abs(pow(z, 3) + x + y)))/(sqrt(pow(x,2) + pow(y,2) - sqrt(z)))
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    // Задание 1
+    /*cout << "Введите х\n";
+    cin >> x;
+    cout << "z = "<< z << "\n";*/
+
+    // Задание 2
+    /*cout << "Введите z\n";
+    cin >> z;
+    cout << "f = " << f << "\n";*/
+
+    // Задание 3
+    /*double a;
+    double b;
+    double y;
+    cout << "Введите a = ";
+    cin >> a;
+    cout << "\nВведите b = ";
+    cin >> b;
+    y = pow(cos(a), 4) + pow(sin(b), 2) + (1 / 4) * pow(sin(2 * a), 2) - 1;
+    cout << "\ny = " << y << "\n";*/
+}
+
